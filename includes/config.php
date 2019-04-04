76d3754bbabe24daf4d4da2bf88c431e11f14ad5
krodriguez93 <rodriguez221293@outlook.com>
2019-04-03 20:53:57 -0500

    aaaa

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', '127.0.0.1' );          // Set database host
+  define( 'DB_USER', 'openshift' );          // Set database user
+  define( 'DB_PASS', 'openshift*' );     // Set database password
+  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+?>
